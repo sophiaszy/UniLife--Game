4d033c46185b9f09b125340d7d06e5e561190b9d
Sophia Shen <sophiashenziyi@gmail.com>
2017-06-11 17:49:45 -0700

    Fixed One Name

--- com/company/model/ScoreKeeper.java
+++ com/company/model/ScoreKeeper.java
@@ -3,5 +3,5 @@ package com.company.model;
 /**
  * Created by SophiaShen on 2017-06-11.
  */
-public class ScoreCounter {
+public class ScoreKeeper {
 }
--- com/company/model/UniLifeGame.java
+++ com/company/model/UniLifeGame.java
@@ -4,4 +4,5 @@ package com.company.model;
  * Created by SophiaShen on 2017-06-11.
  */
 public class UniLifeGame {
+
 }
