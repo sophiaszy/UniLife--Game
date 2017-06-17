6b14ccfa03399ab823bbde9cfee2410ecb5ef31e
amahinpe <anita.mahinpei@cern.ch>
2017-06-17 14:08:01 -0700

    frame

--- com/company/UniLifeGameApp.java
+++ com/company/UniLifeGameApp.java
@@ -1,12 +1,25 @@
 package com.company;
 
+import com.company.model.UniLifeGame;
+
+import javax.swing.*;
+
 /**
  * Created by anita on 6/12/2017.
  */
 
 public class UniLifeGameApp {
 
+    public UniLifeGameApp(){
+        JFrame frame = new JFrame("UniLife Game");
+        frame.setSize(UniLifeGame.WIDTH,UniLifeGame.HEIGHT);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
 	/* write your code here */
+        new UniLifeGameApp();
     }
+
 }
